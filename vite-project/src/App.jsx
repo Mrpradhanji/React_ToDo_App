ae95ca76785c4bc6b538e6558b824c923999fa1d
Mrpradhanji <114905691+Mrpradhanji@users.noreply.github.com>
2024-04-18 23:48:23 +0530

    state changes

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TodoProvider } from './contexts'
 
 function App() {
+  const [todos,setTodos] = useState([]) //by default empty array
+  
+  const addTodo = (todo)=>{
+    setTodos((prev)=>[{id:Date.now(), ...todo}, ...prev])//...prev previous todo
+  }
+
+  const updatedTodo = (id,todo)=>{
+    setTodos((prev)=>prev.map((prevTodo)=>
+    (prevTodo.id===id ? todo : prevTodo)))
+    /*
+    prev.map((eachVal) =>{
+      if(eachVal.id===id){
+        todo
+      }) for understanding
+    } */
+  }
+
+  const deleteTodo =(id)=>{
+    setTodos((prev)=>prev.filter((todo)=>todo.id!==id))
+  }
+
+  const toggleCompleted = (id)=>{
+    setTodos((prev)=>prev.map((prevTodo)=> prevTodo === id ? 
+    {...prevTodo , completed:!prevTodo.completed}:prevTodo))
+  }
+
   return (
-    <div>
-      
+    <TodoProvider value={{todos,addTodo,updatedTodo,deleteTodo,toggleCompleted}}>
+    <div className="bg-[#172842] min-h-screen py-8">
+      <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+        <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+        <div className="mb-4">
+            {/* Todo form goes here */} 
+        </div>
+        <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+        </div>
+      </div>
     </div>
+    </TodoProvider>
   )
 }
 
